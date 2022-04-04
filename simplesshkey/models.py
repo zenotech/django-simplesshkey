904db145339a17ab60c2a54dd86b73a30fec58ed
Mike Turner <mike.turner@zenotech.com>
2022-04-04 14:18:15 +0100

    Update models.py
    Django 3 compatability

--- simplesshkey/models.py
+++ simplesshkey/models.py
@@ -32,7 +32,7 @@
 
 from __future__ import unicode_literals
 
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.db import models
 from django.conf import settings as django_settings
 from django.core.exceptions import ValidationError
